da074913f34fb702e910c908e88181e5f1075c47
Timoteo <timoteo@hdinfo-ma.com>
2026-03-09 22:56:53 -0300

    config: Adição de linhas de importação dos models/tables do projeto.

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # E-commerce de Ballet - MVP
 
+## Link do Design Figma.
+
+https://www.figma.com/design/91jWqf6KQl65SQ3FSUzJC6/eCommerce-Template---SimpleWood-Theme--Full--Magento-2---Community-?node-id=0-1&p=f&t=7szV2DrmqjuVW0f8-0
+
+---
+
 Projeto Flask gerado automaticamente.
 
 ## Executar
--- src/__init__.py
+++ src/__init__.py
@@ -13,10 +13,22 @@ def create_app():
     db.init_app(app)
     migrate.init_app(app, db)
 
-    try:
-        from .routes import register_routes
-        register_routes(app)
-    except Exception:
-        pass
+    # Importações de Models:
+    from src.models.usuario_model import Usuario
+    from src.models.endereco_model import Endereco
+    from src.models.categoria_model import Categoria
+    from src.models.produto_model import Produto
+    from src.models.produto_variavel import Produto_Variavel
+    from src.models.produto_image import Produto_Images
+    from src.models.carrinho_model import Carrinhos
+    from src.models.itens_carrinho import Itens_Carrinho
+    from src.models.ordens import Ordens
+    from src.models.itens_order import Itens_Order
+        
+    # Importações de Blueprintes:
+        
+    from .routes import register_routes
+    register_routes(app)
+
 
     return app

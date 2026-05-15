a07aaa7c295ccb5442dc0a769d7e1d48c3b093ce
Timoteo <timoteo@hdinfo-ma.com>
2026-05-15 16:13:17 -0300

    config: JWT na configurações.

--- src/settings/extensions.py
+++ src/settings/extensions.py
@@ -1,7 +1,18 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager
 from flask_login import LoginManager
+from flask_admin import Admin
+from flask_admin.theme import Bootstrap4Theme
 
 db = SQLAlchemy()
 migrate = Migrate()
+jwt = JWTManager()
 login_manager = LoginManager()
+admin = Admin(
+    name="Painel Administrativo",
+
+    theme=Bootstrap4Theme(
+        swatch="darkly"
+    )
+)
